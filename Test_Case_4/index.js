0a240fc1698a4aeba45f5cdcc73c31ac495a2d09
Zoro <bhattshubham466@gmail.com>
2026-04-25 23:45:29 +0530

    Change health status response from 'OK' to 'healthy'

--- Test_Case_4/index.js
+++ Test_Case_4/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/health', (req, res) => {
-    res.status(200).json({ status: "OK" });
+    res.status(200).json({ status: "healthy" });
 });
 
 app.listen(PORT, () => {
